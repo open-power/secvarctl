1f2ae4b15853c496dd0a65914d8b2dfc39cd376a
Daniel Axtens <dja@axtens.net>
2021-05-05 13:31:34 +1000

    edk2: sync edk2.h with skiboot changes
    There are 3 commits that we want: commit bc54b94fe145
    ("secvar/backend/edk2.h: mark structs as __packed explicitly") plus 2
    posts to the mailing list that fix up the headers and adds the GUID we need.
    
    The other changes are just due to how whitespace has somehow been mangled.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>

--- external/skiboot/libstb/secvar/backend/edk2.h
+++ external/skiboot/libstb/secvar/backend/edk2.h
@@ -41,7 +41,10 @@
 
 #ifndef __EDK2_H__
 #define __EDK2_H__
-#include <ccan/short_types/short_types.h> //added by Nick Child, dja
+
+#include <compiler.h>
+#include <ccan/short_types/short_types.h>
+
 #define UUID_SIZE 16
 
 typedef struct {
@@ -49,14 +52,13 @@ typedef struct {
 } uuid_t;
 
 #define EFI_GLOBAL_VARIABLE_GUID (uuid_t){{0x61, 0xDF, 0xe4, 0x8b, 0xca, 0x93, 0xd2, 0x11, 0xaa, \
-       0x0d, 0x00, 0xe0, 0x98, 0x03, 0x2b, 0x8c}}
+			 0x0d, 0x00, 0xe0, 0x98, 0x03, 0x2b, 0x8c}}
 
 #define EFI_IMAGE_SECURITY_DATABASE_GUID (uuid_t){{0xcb, 0xb2, 0x19, 0xd7, 0x3a, 0x3d, 0x96, 0x45, \
-             0xa3, 0xbc, 0xda, 0xd0, 0x0e, 0x67, 0x65, 0x6f}}
+					   0xa3, 0xbc, 0xda, 0xd0, 0x0e, 0x67, 0x65, 0x6f}}
 
-#define SECVAR_ATTRIBUTES 39
+#define SECVAR_ATTRIBUTES	39
 
-#define EFI_CERT_RSA2048_GUID (uuid_t) {{0xe8,0x66,0x57, 0x3c, 0x9c, 0x26,0x34,  0x4e,  0xaa, 0x14, 0xed, 0x77, 0x6e, 0x85, 0xb3, 0xb6}} //ADDED BY NICK CHILD
 ///
 /// This identifies a signature based on an X.509 certificate. If the signature is an X.509
 /// certificate then verification of the signature of an image should validate the public
@@ -81,25 +83,27 @@ static const uuid_t EFI_CERT_SHA384_GUID = {{ 0x07, 0x53, 0x3e, 0xff, 0xd0, 0x9f
 
 static const uuid_t EFI_CERT_SHA512_GUID = {{ 0xae, 0x0f, 0x3e, 0x09, 0xc4, 0xa6, 0x50, 0x4f, 0x9f, 0x1b, 0xd4, 0x1e, 0x2b, 0x89, 0xc1, 0x9a }};
 
-#define EFI_VARIABLE_NON_VOLATILE       0x00000001
-#define EFI_VARIABLE_BOOTSERVICE_ACCESS       0x00000002
-#define EFI_VARIABLE_RUNTIME_ACCESS       0x00000004
+static const uuid_t EFI_CERT_RSA2048_GUID = {{ 0xe8, 0x66, 0x57, 0x3c, 0x9c, 0x26, 0x34, 0x4e, 0xaa, 0x14, 0xed, 0x77, 0x6e, 0x85, 0xb3, 0xb6 }};
+
+#define EFI_VARIABLE_NON_VOLATILE				0x00000001
+#define EFI_VARIABLE_BOOTSERVICE_ACCESS				0x00000002
+#define EFI_VARIABLE_RUNTIME_ACCESS				0x00000004
 
 /*
  * Attributes of Authenticated Variable
  */
-#define EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS  0x00000020
-#define EFI_VARIABLE_APPEND_WRITE       0x00000040
+#define EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS	0x00000020
+#define EFI_VARIABLE_APPEND_WRITE				0x00000040
 /*
  * NOTE: EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS is deprecated and should be
  * considered reserved.
  */
-#define EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS     0x00000010
+#define EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS			0x00000010
 
 /*
  * win_certificate.w_certificate_type
  */
-#define WIN_CERT_TYPE_PKCS_SIGNED_DATA  0x0002
+#define WIN_CERT_TYPE_PKCS_SIGNED_DATA	0x0002
 
 #define SECURE_BOOT_MODE_ENABLE           1
 #define SECURE_BOOT_MODE_DISABLE          0
@@ -121,17 +125,17 @@ static const uuid_t EFI_CERT_SHA512_GUID = {{ 0xae, 0x0f, 0x3e, 0x09, 0xc4, 0xa6
  *   TimeZone:   -1440 to 1440 or 2047
  */
 struct efi_time {
-  u16 year;
-  u8 month;
-  u8 day;
-  u8 hour;
-  u8 minute;
-  u8 second;
-  u8 pad1;
-  u32 nanosecond;
-  s16 timezone;
-  u8 daylight;
-  u8 pad2;
+	u16 year;
+	u8 month;
+	u8 day;
+	u8 hour;
+	u8 minute;
+	u8 second;
+	u8 pad1;
+	u32 nanosecond;
+	s16 timezone;
+	u8 daylight;
+	u8 pad2;
 };
 //***********************************************************************
 // Signature Database
@@ -139,9 +143,8 @@ struct efi_time {
 ///
 /// The format of a signature database.
 ///
-#pragma pack(1)
 
-typedef struct {
+typedef struct __packed {
   ///
   /// An identifier which identifies the agent which added the signature to the list.
   ///
@@ -152,7 +155,7 @@ typedef struct {
   unsigned char SignatureData[0];
 } EFI_SIGNATURE_DATA;
 
-typedef struct {
+typedef struct __packed {
   ///
   /// Type of the signature. GUID signature types are defined in below.
   ///
@@ -160,15 +163,15 @@ typedef struct {
   ///
   /// Total size of the signature list, including this header.
   ///
-  uint32_t  SignatureListSize;
+  uint32_t	SignatureListSize;
   ///
   /// Size of the signature header which precedes the array of signatures.
   ///
-  uint32_t  SignatureHeaderSize;
+  uint32_t	SignatureHeaderSize;
   ///
   /// Size of each signature.
   ///
-  uint32_t  SignatureSize;
+  uint32_t	SignatureSize;
   ///
   /// Header before the array of signatures. The format of this header is specified
   /// by the SignatureType.
@@ -184,50 +187,51 @@ typedef struct {
  * The win_certificate structure is part of the PE/COFF specification.
  */
 struct win_certificate {
-  /*
-   * The length of the entire certificate, including the length of the
-   * header, in bytes.
-   */
-  u32  dw_length;
-  /*
-   * The revision level of the WIN_CERTIFICATE structure. The current
-   * revision level is 0x0200.
-   */
-  u16  w_revision;
-  /*
-   * The certificate type. See WIN_CERT_TYPE_xxx for the UEFI certificate
-   * types. The UEFI specification reserves the range of certificate type
-   * values from 0x0EF0 to 0x0EFF.
-   */
-  u16  w_certificate_type;
-  /*
-   * The following is the actual certificate. The format of
-   * the certificate depends on wCertificateType.
-   */
-  /// UINT8 bCertificate[ANYSIZE_ARRAY];
-};
+	/*
+	 * The length of the entire certificate, including the length of the
+	 * header, in bytes.
+	 */
+	u32  dw_length;
+	/*
+	 * The revision level of the WIN_CERTIFICATE structure. The current
+	 * revision level is 0x0200.
+	 */
+	u16  w_revision;
+	/*
+	 * The certificate type. See WIN_CERT_TYPE_xxx for the UEFI certificate
+	 * types. The UEFI specification reserves the range of certificate type
+	 * values from 0x0EF0 to 0x0EFF.
+	 */
+	u16  w_certificate_type;
+	/*
+	 * The following is the actual certificate. The format of
+	 * the certificate depends on wCertificateType.
+	 */
+	/// UINT8 bCertificate[ANYSIZE_ARRAY];
+} __packed;
 
 /*
  * Certificate which encapsulates a GUID-specific digital signature
  */
 struct win_certificate_uefi_guid {
-  /*
-   * This is the standard win_certificate header, where w_certificate_type
-   * is set to WIN_CERT_TYPE_EFI_GUID.
-   */
-  struct win_certificate hdr;
-  /*
-   * This is the unique id which determines the format of the cert_data.
-   */
-  uuid_t cert_type;
-  /*
-   * The following is the certificate data. The format of the data is
-   * determined by the @cert_type. If @cert_type is
-   * EFI_CERT_TYPE_RSA2048_SHA256_GUID, the @cert_data will be
-   * EFI_CERT_BLOCK_RSA_2048_SHA256 structure.
-   */
-  u8 cert_data[];
-};
+	/*
+	 * This is the standard win_certificate header, where w_certificate_type
+	 * is set to WIN_CERT_TYPE_EFI_GUID.
+	 */
+	struct win_certificate hdr;
+	/*
+	 * This is the unique id which determines the format of the cert_data.
+	 */
+	uuid_t cert_type;
+	/*
+	 * The following is the certificate data. The format of the data is
+	 * determined by the @cert_type. If @cert_type is
+	 * EFI_CERT_TYPE_RSA2048_SHA256_GUID, the @cert_data will be
+	 * EFI_CERT_BLOCK_RSA_2048_SHA256 structure.
+	 */
+	u8 cert_data[];
+} __packed;
+
 /*
  * When the attribute EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS is set,
  * then the Data buffer shall begin with an instance of a complete (and
@@ -238,15 +242,15 @@ struct win_certificate_uefi_guid {
  * calls to GetVariable().
  */
 struct efi_variable_authentication_2 {
-  /*
-   * For the TimeStamp value, components Pad1, Nanosecond, TimeZone, Daylight and
-   * Pad2 shall be set to 0. This means that the time shall always be expressed in GMT.
-   */
-  struct efi_time timestamp;
-  /*
-   * Only a CertType of  EFI_CERT_TYPE_PKCS7_GUID is accepted.
-   */
-  struct win_certificate_uefi_guid auth_info;
-};
+	/*
+	 * For the TimeStamp value, components Pad1, Nanosecond, TimeZone, Daylight and
+	 * Pad2 shall be set to 0. This means that the time shall always be expressed in GMT.
+	 */
+	struct efi_time timestamp;
+	/*
+	 * Only a CertType of  EFI_CERT_TYPE_PKCS7_GUID is accepted.
+	 */
+	struct win_certificate_uefi_guid auth_info;
+} __packed;
 
 #endif
