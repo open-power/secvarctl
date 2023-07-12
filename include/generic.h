41d359567a52aff7fe018e9324015b8a55bb3bae
Eric Richter <erichte@linux.ibm.com>
2023-07-12 15:33:13 -0500

    generic: remove unusued function get_leading_whitespace
    Signed-off-by: Eric Richter <erichte@linux.ibm.com>

--- include/generic.h
+++ include/generic.h
@@ -17,7 +17,6 @@ int write_data_to_file(const char *file, const char *buff, size_t size);
 int create_file(const char *file, const char *buff, size_t size);
 void print_raw(const char *c, size_t size);
 int is_file(const char *path);
-size_t get_leading_whitespace(unsigned char *data, size_t dataSize);
 void print_hex(unsigned char *data, size_t length);
 int realloc_array(void **arr, size_t new_length, size_t size_each);
 
