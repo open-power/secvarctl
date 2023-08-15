283ff7ad8adfe51f3ffb9375b19b6e3fc2d77734
Eric Richter <erichte@linux.ibm.com>
2023-08-15 15:07:15 -0500

    test/common: remove memcheck
    Since the original valgrind runner has been replaced by ASAN, the old
    memcheck logic, args, etc can be removed.
    
    The getCmdResult() function can probably be merged with command(),
    unless there is a need for them to remain separate.
    
    Signed-off-by: Eric Richter <erichte@linux.ibm.com>

--- test/common.py
+++ test/common.py
@@ -1,9 +1,6 @@
 import unittest
 import subprocess
 
-MEMCHECK = False
-MEM_ERR = 101
-
 
 class SecvarctlTest(unittest.TestCase):
     out = ""
@@ -21,25 +18,9 @@ class SecvarctlTest(unittest.TestCase):
                 return result
         return subprocess.call(args, stdout=out, stderr=out)
 
+    # TODO: slated for removal or merge with command
     def getCmdResult(self, args, out):
-        if MEMCHECK:
-            mem_cmd = [
-                "valgrind",
-                "-q",
-                "--error-exitcode=" + str(MEM_ERR),
-                "--leak-check=full",
-            ] + args
-            with open(out, "w") as f:
-                f.write("\n\n**********COMMAND RAN: $" + " ".join(mem_cmd) + "\n")
-                result = subprocess.call(mem_cmd, stdout=f, stderr=f)
-                f.close()
-                self.assertNotEqual(result, MEM_ERR)
-        # we run twice because valgrind interprets a -1 return code as a 0, which stinks
-        rc = self.command(args, out)
-        if rc == 0:
-            return True
-        else:
-            return False
+        return bool(self.command(args, out))
 
     def setupTestEnvironment(self):
         for var in ["out", "test_env_dir", "test_data_dir", "log_dir"]:
