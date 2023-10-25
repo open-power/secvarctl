772fd918066baf02af4b18fd1d1e40e813c642f2
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2023-10-25 16:18:51 +0530

    guest: add timestamp print output to read/verify commands
    Move the existing print_timestamp function to common/util, and add a new
    helper to handle reading and printing a timestamp from a variable buffer.
    
    Prints the timestamp when using the read command, and when using verbose
    levels in the verify command.
    
    This commit is part of a larger work in progress on factoring out
    loading data from a variable vs a file, see more information in #76.
    
    Co-developed-by: Eric Richter <erichte@linux.ibm.com>
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>

--- backends/guest/common/util.c
+++ backends/guest/common/util.c
@@ -31,6 +31,27 @@ const struct signature_type_info signature_type_list[] = {
 static uint8_t append[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
 static uint8_t replace[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
+void print_timestamp(timestamp_t t)
+{
+	printf("%04d-%02d-%02d %02d:%02d:%02d UTC\n", t.year, t.month, t.day, t.hour, t.minute,
+	       t.second);
+}
+
+void read_timestamp(const uint8_t *esl_data)
+{
+	timestamp_t timestamp;
+
+	if (esl_data == NULL)
+		return;
+
+	// Special case: data read in from firmware contains a 16-bytes header, containing
+	//  a one-byte version number, then 15 bytes of timestamp -- truncating the trailing
+	//  padding byte at the end of the struct.
+	memcpy(&timestamp, esl_data + 1, TIMESTAMP_LEN - 1);
+	printf("\tTimestamp: ");
+	print_timestamp(timestamp);
+}
+
 /*
  * creates the append header using append flag
  */
--- backends/guest/common/verify.c
+++ backends/guest/common/verify.c
@@ -71,6 +71,7 @@ static int update_variable(const char *variable_name, const uint8_t *auth_data,
 		prlog(PR_INFO, "\tappend update: %s\n\n", (append_update ? "True" : "False"));
 
 		if (*new_esl_data != NULL) {
+			read_timestamp(*new_esl_data);
 			rc = print_esl_buffer((*new_esl_data + TIMESTAMP_LEN),
 					      (*new_esl_data_size - TIMESTAMP_LEN), variable_name);
 			if (rc != SUCCESS)
@@ -95,13 +96,15 @@ static int get_current_esl_data(const char *esl_file, uint8_t **current_esl_data
 		return INVALID_FILE;
 
 	buffer = get_data_from_file(esl_file, SIZE_MAX, &buffer_size);
-	if (buffer != NULL) {
+	if (buffer != NULL && buffer_size >= TIMESTAMP_LEN) {
 		if (buffer_size == DEFAULT_PK_LEN) {
 			print_raw(buffer, buffer_size);
 			free(buffer);
 			buffer = NULL;
 			buffer_size = 0;
-		} else if (buffer_size != TIMESTAMP_LEN) {
+		} else {
+			if (verbose >= PR_INFO)
+				read_timestamp(buffer);
 			rc = validate_esl(buffer + TIMESTAMP_LEN, buffer_size - TIMESTAMP_LEN);
 			if (rc != SUCCESS) {
 				free(buffer);
